bdea2ae0e3c632e15bb9387a2039acd6d6e0432b
Verity Hill <verity.lowenna@gmail.com>
2018-12-24 20:41:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = { Avantasia: "The Glory of Rome"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
+<<<<<<< HEAD
   Object.assign(playlist, {[artistName]: songTitle});
   return playlist;
 }
@@ -8,5 +9,12 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
+=======
+  return playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+>>>>>>> 719366828d7141a9174a2d5da4450149833292e3
   return playlist;
 }
